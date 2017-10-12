2ad9339a6ce07094175df17a803cb5a807070af1
Lucas Tomaz <lucastomazafonso@gmail.com>
2017-10-11 21:27:47 -0300

    Adição de cabeçalho

--- Program.cs
+++ Program.cs
@@ -18,9 +18,24 @@ namespace arquivocsv
             Console.WriteLine("Digite a sua idade: ");
             idade = Int16.Parse(Console.ReadLine());
 
-
-            StreamWriter arquivo = new StreamWriter("dados.csv", true);
+            FileInfo fi = new FileInfo("dados_cabecalho.csv");
+            
+            //O ponto de exclamação significa negação, ou seja, diz que tal arquivo "não é".
+            
+           
+            StreamWriter arquivo;
+            
+            if (fi.Exists){
+            arquivo = new StreamWriter("dados_cabecalho.csv", true);
             arquivo.WriteLine(nome+";"+email+";"+idade+" anos;"+DateTime.Now.ToShortDateString());
+            }
+            else {
+            arquivo = new StreamWriter("dados_cabecalho.csv", true);
+            arquivo.WriteLine("Nome;Email;Idade;Data de cadastro");
+            arquivo.WriteLine(nome+";"+email+";"+idade+" anos;"+DateTime.Now.ToShortDateString());
+            }
+
+
 
             arquivo.Close();
 
